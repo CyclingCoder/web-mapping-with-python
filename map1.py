0f0287150a39ee741ae3539ebf9a6d09e592d3ff
CyclingCoder <jenny.smith93@icloud.com>
2022-03-03 12:36:32 -0700

    added elev in meters

--- map1.py
+++ map1.py
@@ -1,9 +1,19 @@
 import folium
+import pandas
+
+data= pandas.read_csv("Volcanoes.txt")
+lat = list(data['LAT'])
+lon = list(data['LON'])
+elev= list(data["ELEV"])
+
+def color_producer(elevation):
+    return 'green'
+
 map = folium.Map(location= [38.58, -99.09], zoom_start=6, tiles= "Stamen Terrain")
 fg =folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="HI I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(str(el)+ "m", parse_html=True), icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
