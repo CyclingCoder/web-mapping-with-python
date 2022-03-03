cf856403cef55da15395c75838ea0b922addcad4
CyclingCoder <jenny.smith93@icloud.com>
2022-03-03 13:19:39 -0700

    adding population color

--- map1.py
+++ map1.py
@@ -20,6 +20,10 @@ fg =folium.FeatureGroup(name="My Map")
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius =6, popup=str(el) + 'm', fill_color=color_producer(el), color = 'grey', fill=True, fill_opacity = 0.7))
 
+
+fg.add_child(folium.GeoJson(data= open('world.json', 'r', encoding= 'utf-8-sig').read(),
+ style_function=lambda x:{'fillColor': 'green' if x['properties']['POP2005'] < 1000000
+ else 'blue' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 map.add_child(fg)
 
 map.save("Map1.html")
